4e815b6f7f97e0f87b61c1627821948c03fe5cc5
k-kurikuri <kurimixgoo@gmail.com>
2020-02-05 12:31:20 +0900

    Add go.mod & main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/k-kurikuri/slack-webhook-go
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	os.Exit(realMain())
+}
+
+func realMain() int {
+	return 0
+}
